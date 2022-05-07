13c8ce483685afdf229dd93976629a3c14ad4e8f
Dipirona537 <dipirona28@gmail.com>
2022-05-07 16:05:42 -0400

    melhoras

--- divisor.h
+++ divisor.h
@@ -13,6 +13,10 @@ void divisor(const string& equacao) {
     string operadores;
     string quebras;
     string valor_final;
+    int posi[equacao.size()];
+    float valor_float;
+    int op_posicao[equacao.size()];
+    int ponto_int[equacao.size()];
     int tam = equacao.size();
     string ponto;
     const string alfabeto = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHYJKLMNOPQRSTUVWXYZ!@#$%&()_=;:'[]{},.<>?|\\";
@@ -35,38 +39,57 @@ void divisor(const string& equacao) {
 
     }
     for (int i = 0; i < equacao.size(); i++) {
+        for (char k: alfabeto) {
+            if (ponto[i] == k) {
+                posi[i] = i;
+                ponto_int[i] = k - '0';
+                cout << "Numero ou simbolo = " << ponto_int[i] << ", Posicao = " << posi[i] << endl;
+            }
+        }
+    }
+    for (int i = 0; i < equacao.size(); i++) {
+        /*for (char k : alfabeto) {
+            if (ponto[i] == k) {
+                ponto_int[i] = k - '0';
+                cout << "Numero ou simbolo = " << ponto_int[i] << ", Posicao = " << i << endl;
+            }
+        }*/
         for (char k: pontos) {
             if (ponto[i] == k) {
                 switch (ponto[i]) {
                     case '+':
                         operadores = to_string(k+958);
-                        cout << operadores << " Operador " << k << " Posicao = " << i << endl;
+                        op_posicao[i] += i;
+                        valor_float = (float)ponto_int[i-1] + (float)ponto_int[i+1];
+                        cout << operadores << " Operador " << k << " Posicao = " << op_posicao[i] << endl << " Valor da op " << k << " no ponto " << i << " = " << valor_float << endl;
                         break;
                     case '-':
                         operadores = to_string(k+965);
-                        cout << operadores << " Operador " << k << " Posicao = " << i << endl;
+                        op_posicao[i] = i;
+                        valor_float = (float)ponto_int[i-1] - (float)ponto_int[i+1];
+                        cout << operadores << " Operador " << k << " Posicao = " << op_posicao[i] << endl << " Valor da op " << k << " no ponto " << i << " = " << valor_float << endl;
                         //cout << "conta de menos" << endl;
                         break;
                     case '/':
                         operadores = to_string(k+964);
-                        cout << operadores << " Operador " << k << " Posicao = " << i << endl;
+                        op_posicao[i] = i;
+                        valor_float = (float)ponto_int[i-1] / (float)ponto_int[i+1];
+                        cout << operadores << " Operador " << k << " Posicao = " << op_posicao[i] << endl << " Valor da op " << k << " no ponto " << i << " = " << valor_float << endl;
                         break;
                     case '*':
                         operadores = to_string(k+1058);
-                        cout << operadores << " Operador " << k << " Posicao = " << i << endl;
+                        op_posicao[i] = i;
+                        valor_float = (float)ponto_int[i-1] * (float)ponto_int[i+1];
+                        cout << operadores << " Operador " << k << " Posicao = " << op_posicao[i] << endl << " Valor da op " << k << " no ponto " << i << " = " << valor_float << endl;
                         break;
                     case '^':
+                        op_posicao[i] = i;
                         operadores = to_string(k+1016);
-                        cout << operadores << " Operador " << k << " Posicao = " << i << endl;
+                        valor_float = pow((float)ponto_int[i-1], (float)ponto_int[i+1]);
+                        cout << operadores << " Operador " << k << " Posicao = " << op_posicao[i] << endl << " Valor da op " << k << " no ponto " << i << " = " << valor_float << endl;
                         break;
                 }
             }
-        }
-    }
-    for (int i = 0; i < equacao.size(); i++) {
-        for (char k: alfabeto) {
-            if (ponto[i] == k)
-                cout << "Numero ou simbolo = " << k << ", Posicao = " << i << endl;
         }
     }
 }
